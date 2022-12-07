dfe1ec2472b9b8f73ed6dd0afbeae44a3983322e
AT <hypothetical.andrei@gmail.com>
2022-12-07 17:37:09 +0200

    .

--- s4/simple-server/server.js
+++ s4/simple-server/server.js
@@ -8,10 +8,16 @@ const sequelize = new Sequelize({
 
 const Kitten = sequelize.define('kitten', {
   name: Sequelize.STRING,
-  color: Sequelize.STRING
+  color: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: [3, 10]
+    }
+  }
 })
 
-sequelize.sync()
+sequelize.sync({ alter: true })
   .then(() => {
     console.log('tables created')
   })
@@ -23,29 +29,29 @@ app.use((req, res, next) => {
   next()
 })
 
+app.use(express.static('public'))
+
 app.use(express.json())
 
-app.get('/kittens', async (req, res) => {
+app.get('/kittens', async (req, res, next) => {
   try {
     const kittens = await Kitten.findAll()
     res.status(200).json(kittens)
   } catch (error) {
-    console.warn(error)
-    res.status(500).json({ message: 'kitten error' })
+    next(error)
   }
 })
 
-app.post('/kittens', async (req, res) => {
+app.post('/kittens', async (req, res, next) => {
   try {
     const kitten = await Kitten.create(req.body)
     res.status(201).json(kitten)
   } catch (error) {
-    console.warn(error)
-    res.status(500).json({ message: 'kitten error' })
+    next(error)
   }
 })
 
-app.get('/kittens/:id', async (req, res) => {
+app.get('/kittens/:id', async (req, res, next) => {
   try {
     const kitten = await Kitten.findByPk(req.params.id)
     if (kitten) {
@@ -53,45 +59,45 @@ app.get('/kittens/:id', async (req, res) => {
     } else {
       res.status(404).json({ message: 'not found' })
     }
-      
   } catch (error) {
-    console.warn(error)
-    res.status(500).json({ message: 'kitten error' })
+    next(error)
   }
 })
 
-app.put('/kittens/:id', async (req, res) => {
+app.put('/kittens/:id', async (req, res, next) => {
   try {
     const kitten = await Kitten.findByPk(req.params.id)
-    kitten.color = req.body.color
-    kitten.name = req.body.name
-    await kitten.save()
     if (kitten) {
+      kitten.color = req.body.color
+      kitten.name = req.body.name
+      await kitten.save()
+      res.status(202).json({ message: 'accepted' })
+    } else {
+      res.status(404).json({ message: 'not found' })
+    }      
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.delete('/kittens/:id', async (req, res, next) => {
+  try {
+    const kitten = await Kitten.findByPk(req.params.id)
+    if (kitten) {
+      await kitten.destroy()
       res.status(202).json({ message: 'accepted' })
     } else {
       res.status(404).json({ message: 'not found' })
     }
       
   } catch (error) {
-    console.warn(error)
-    res.status(500).json({ message: 'kitten error' })
+    next(error)
   }
 })
 
-app.delete('/kittens/:id', async (req, res) => {
-  try {
-    const kitten = await Kitten.findByPk(req.params.id)
-    await kitten.destroy()
-    if (kitten) {
-      res.status(202).json({ message: 'accepted' })
-    } else {
-      res.status(404).json({ message: 'not found' })
-    }
-      
-  } catch (error) {
-    console.warn(error)
-    res.status(500).json({ message: 'kitten error' })
-  }
+app.use((err, req, res, next) => {
+  console.warn(err)
+  res.status(500).json({ message: 'server error' })
 })
 
 app.listen(8080)
